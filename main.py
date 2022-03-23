864171935e8c7b4f6ef43ecc5b81f9c0fd7e5bc5
jaroslawbujakiewicz <jarek9002@gmail.com>
2022-03-23 14:14:50 +0100

    init

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, send_from_directory
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/error_internal')
+def error_internal():
+    return render_template('template.html', name='ERROR 505'), 505
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
